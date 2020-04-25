a88097817470a1f43c407b575b0d4881ea06f891
lipingh <lipingh@andrew.cmu.edu>
2020-04-24 17:50:12 -0700

    part 3

---
+++ corr.R
@@ -0,0 +1,14 @@
+corr <- function(directory, threshold = 0) {
+  # Use complete function in part 2 to calulate number of complete cases
+  df <- complete(directory)
+  filtered_df <- subset(df, df$no > threshold)
+  correlation <- c()
+  for (i in filtered_df$id) {
+    file_path <- paste(getwd(), "/", directory, "/", sprintf("%03d", i), ".csv", sep = "")
+    data <- read.csv(file_path)
+    #remove NA, otherwise the result will NA
+    data <- subset(data, complete.cases(data))
+    correlation <- c(correlation, cor(data$nitrate, data$sulfate))
+  }
+  correlation
+}
