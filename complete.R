caf618d26d79d7a3cc87effc826709951b6c80da
lipingh <lipingh@andrew.cmu.edu>
2020-04-24 17:04:02 -0700

    complete function

---
+++ complete.R
@@ -0,0 +1,11 @@
+complete <- function (directory, id = 1: 332) {
+  file_name <- c()
+  number_cases <- c()
+  for (i in id) {
+    file_path <- paste(getwd(), "/", directory, "/", sprintf("%03d", i), ".csv", sep = "")
+    data <- read.csv(file_path)
+    file_name <- c(file_name, i)
+    number_cases <- c(number_cases, sum(complete.cases(data)))
+  }
+  data.frame(id = file_name, no = number_cases)
+}
